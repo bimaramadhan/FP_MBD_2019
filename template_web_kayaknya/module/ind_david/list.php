35fbf93ba65bd237ed4b04b6df303c4ceb872b56
csrausyan <43034118+csrausyan@users.noreply.github.com>
2019-05-17 07:37:43 +0700

    template (default david)
    tolng bro

---
+++ template_web_kayaknya/module/ind_david/list.php
@@ -0,0 +1,161 @@
+<?php
+
+/**
+ * template, kurang lebih kyk gini nanti
+ * kalo ada apa2 figure-out sendiri tolong bro plis bro
+ * aku juga belom bikin punyaku plis bro ga becanda bro
+ * oh god oh fucj
+ * 
+ */
+
+//echo "bruh";
+echo "<div id= individual>";
+
+echo "<h1>Tugas Individu - David Laksamana</h1>
+    <br><h2>View</h2>";
+
+//bagian ini buat VIEW, xxxxxx nya diganti ya.
+echo "<h3>1. xxxxxx </h3>";
+//$queryview1 = mysqli_query($koneksi,"SELECT * from barang");
+    //query nya diganti sesuai yg dikerjain.
+
+//ini aaaaa, bbbbb, ccccc nya ganti
+echo "<table class='table-list'>";
+echo "<tr class='baris-title'>
+        <th class='kolom-nomor'>No</th>
+        <th class='kiri'>aaaa</th>
+        <th class='kiri'>bbbb</th>
+        <th class='kiri'>cccc</th>
+     </tr>";
+
+$noview1 = 1;
+//nanti uncomment aja
+//while($row=mysqli_fetch_assoc($queryview1)){}
+
+    /** 
+     *  ini juga ganti aaaa bbbb ccccnya
+     *  jadi $row[nama_kolom_yang_ada_di_viewnya]
+     * 
+    */
+echo "<tr>
+        <td class='kolom-nomor'>$noview1</td>
+        <td class='kiri'>aaaaaa</td>
+        <td class='kiri'>bbbbbb</td>
+        <td class='kiri'>cccccc</td>
+        ";
+echo "</table>";
+
+echo "<h3>1. yyyyyyyyy </h3>";
+echo "<table class='table-list'>";
+echo "<tr class='baris-title'>
+        <th class='kolom-nomor'>No</th>
+        <th class='kiri'>aaaa</th>
+        <th class='kiri'>bbbb</th>
+        <th class='kiri'>cccc</th>
+     </tr>";
+
+$noview1 = 1;
+//nanti uncomment aja
+//while($row=mysqli_fetch_assoc($queryview1)){}
+
+    /** 
+     *  ini juga ganti aaaa bbbb ccccnya
+     *  jadi $row[nama_kolom_yang_ada_di_viewnya]
+     * 
+    */
+echo "<tr>
+        <td class='kolom-nomor'>$noview1</td>
+        <td class='kiri'>aaaaaa</td>
+        <td class='kiri'>bbbbbb</td>
+        <td class='kiri'>cccccc</td>
+        ";
+echo "</table>";
+
+//ini buat fungsi
+echo "<br><h2>Function</h2>";
+echo "<h3>1. zxczczxc </h3>";
+//isi ini
+echo "<h3>2. gfdfdfgf </h3>";
+//isi ini juga
+
+//ini buat prosedur
+echo "<br><h2>Procedure</h2>";
+echo "<h3>1. iiyyui </h3>";
+//isi ini
+echo "<h3>2. tytvnnb </h3>";
+//isi ini juga
+
+//ini buat trigger
+echo "<br><h2>Trigger</h2>";
+echo "<h3>1. pjkghj </h3>";
+//isi ini
+echo "<h3>2. xxxxxx </h3>";
+//isi ini juga
+
+//ini buat join
+echo "<br><h2>Join</h2>";
+//join berarti tabel kan? ini ngopas dari view
+echo "<h3>1. xxxxxx </h3>";
+//$queryview1 = mysqli_query($koneksi,"SELECT * from barang");
+    //query nya diganti sesuai yg dikerjain.
+
+//ini aaaaa, bbbbb, ccccc nya ganti
+echo "<table class='table-list'>";
+echo "<tr class='baris-title'>
+        <th class='kolom-nomor'>No</th>
+        <th class='kiri'>aaaa</th>
+        <th class='kiri'>bbbb</th>
+        <th class='kiri'>cccc</th>
+     </tr>";
+
+$noview1 = 1;
+//nanti uncomment aja
+//while($row=mysqli_fetch_assoc($queryview1)){}
+
+    /** 
+     *  ini juga ganti aaaa bbbb ccccnya
+     *  jadi $row[nama_kolom_yang_ada_di_viewnya]
+     * 
+    */
+echo "<tr>
+        <td class='kolom-nomor'>$noview1</td>
+        <td class='kiri'>aaaaaa</td>
+        <td class='kiri'>bbbbbb</td>
+        <td class='kiri'>cccccc</td>
+        ";
+echo "</table>";
+
+echo "<h3>1. yyyyyyyyy </h3>";
+echo "<table class='table-list'>";
+echo "<tr class='baris-title'>
+        <th class='kolom-nomor'>No</th>
+        <th class='kiri'>aaaa</th>
+        <th class='kiri'>bbbb</th>
+        <th class='kiri'>cccc</th>
+     </tr>";
+
+$noview1 = 1;
+//nanti uncomment aja
+//while($row=mysqli_fetch_assoc($queryview1)){}
+
+    /** 
+     *  ini juga ganti aaaa bbbb ccccnya
+     *  jadi $row[nama_kolom_yang_ada_di_viewnya]
+     * 
+    */
+echo "<tr>
+        <td class='kolom-nomor'>$noview1</td>
+        <td class='kiri'>aaaaaa</td>
+        <td class='kiri'>bbbbbb</td>
+        <td class='kiri'>cccccc</td>
+        ";
+echo "</table>";
+
+
+//ini buat fungsi
+echo "<br><h2>Function-based Index</h2>";
+echo "<h3>zxczczxc </h3>";
+//isi ini
+
+echo "</div>";
+?>
